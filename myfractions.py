b00978eab284c1a04c069c91a76eee91322613bc
swamicharanrao <65893887+swamicharanrao@users.noreply.github.com>
2020-06-04 17:38:36 +0530

    Add files via upload

---
+++ myfractions.py
@@ -0,0 +1,26 @@
+def fr(a,b):
+    c=a/b
+    print("fraction is:",c)
+
+
+def add(a,b,c,d):
+    e1=a/b
+    f1=c/d
+    print("Addition is :",e1+f1)
+    
+    
+def sub(a,b,c,d):
+    e2=a/b
+    f2=c/d
+    print("subtraction is :",e2-f2)
+    
+    
+def mul(a,b,c,d):
+    e3=a/b
+    f3=c/d
+    print("Multiplication is :",e3*f3)
+    
+def div(a,b,c,d):
+    e4=a/b
+    f4=c/d
+    print("Division is :",e4/f4)
